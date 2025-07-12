0ac9683996487ab12b0424525f51496ccfb61fa4
ifee90 <ifeomaoguejiofor90@gmail.com>
2025-07-12 17:02:08 +0100

    My Simple Interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -4,7 +4,7 @@ rate = 0.05
 time = 3
 
 #calculating the simple interest 
-interest = (principal * rate * time)
+interest = principal * rate * time
 
 #print result
 print(f"The simple interest is: {interest}")
