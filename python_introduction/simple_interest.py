8da80b19627577e876b30ae075725b813f3fc27e
ifee90 <ifeomaoguejiofor90@gmail.com>
2025-07-12 16:54:53 +0100

    My Simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#define the three variables in this file
+principal = 1000
+rate = 0.05
+time = 3
+
+#calculating the simple interest 
+interest = (principal * rate * time)
+
+#print result
+print(f"The simple interest is: {interest}")
